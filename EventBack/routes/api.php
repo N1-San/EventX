a81ffe744ccde2c97530c63386e0c08bbed6b3b8
Aniki <33519431+N1-San@users.noreply.github.com>
2025-03-16 21:32:13 +0530

    auth route setup

--- EventBack/routes/api.php
+++ EventBack/routes/api.php
@@ -3,6 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\GuestController;
+use App\Http\Controllers\VendorController;
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('events', EventController::class);
+    Route::apiResource('tasks', TaskController::class);
+    Route::apiResource('guests', GuestController::class);
+    Route::apiResource('vendors', VendorController::class);
 });
