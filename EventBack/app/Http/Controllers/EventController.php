79d49f41ef25eb7e02a02ae4282b8bfa462ad044
Aniki <33519431+N1-San@users.noreply.github.com>
2025-03-16 21:32:24 +0530

    event controller setup

--- EventBack/app/Http/Controllers/EventController.php
+++ EventBack/app/Http/Controllers/EventController.php
@@ -7,59 +7,38 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        return Event::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'expected_occupancy' => 'required|integer',
+            'type' => 'required|string',
+            'venue' => 'required|string',
+            'date' => 'required|date',
+        ]);
+
+        return Event::create($validated);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Event $event)
     {
-        //
+        return $event;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Event $event)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Event $event)
     {
-        //
+        $event->update($request->all());
+        return $event;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Event $event)
     {
-        //
+        $event->delete();
+        return response()->json(['message' => 'Deleted successfully']);
     }
 }
